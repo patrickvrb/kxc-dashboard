7751fb8a2831c359f530234c6f3d16fa9d2df066
patrickvrb <patrickvrb@gmail.com>
2021-03-11 11:21:19 -0300

    Setando angulo referência e printando no console o angulo variado

--- dashboard.py
+++ dashboard.py
@@ -1,38 +1,44 @@
-import sys
-import random
+from PyQt5 import QtWidgets, QtCore
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+from random import randint
 from serialcoms import SerialIO
-from PyQt5 import QtCore, QtWidgets, QtChart
-from PyQt5.Qt import Qt 
-from PyQt5.QtGui import QPainter
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setup()
-    
-    def setup(self):
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
         self.serial = SerialIO()
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setWindowTitle('KXC Dashboard')
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
 
-        self.button.clicked.connect(self.magic)
-        self.button.resize(self.button.sizeHint())
+        self.graphWidget.setBackground('w')
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
         
-    def magic(self):
-        self.text.setText(self.serial.read())
-        # self.text.setText(random.choice(self.hello))
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first 
+        self.y.append( randint(0,100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
+
         
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
-    widget = MyWidget()
-    widget.resize(600, 400)
-    widget.show()
-
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
     sys.exit(app.exec_())
--- serialcoms.py
+++ serialcoms.py
@@ -1,7 +1,6 @@
-import sys
+from math import acos, sqrt
 import serial
 from time import sleep
-
 
 class SerialIO():
     def __init__(self):
@@ -13,6 +12,39 @@ class SerialIO():
         self.arduinoData.write('4'.encode('utf-8')) # Forçando modo 4 (MPU)
         self.arduinoData.write('\n'.encode('utf-8'))
         self.arduinoData.flushOutput()
+    
+    def get_integer_value(self, str_value):
+        if str_value.startswith('+'):
+            return int(str_value[1:])
+        else:
+            return int(str_value)
+
+    def get_x_y_z(self,  input_str):
+        var_list = input_str.split(' ')[:-1]
+        x, y, z = [self.get_integer_value(value) for value in var_list]
+        return x, y, z
 
     def read(self):
-        return self.arduinoData.read_all().decode('utf-8')
+        reference_vector = None
+        current_vector = None
+        while True:
+            normalized_buffer = self.arduinoData.read_until().decode('utf-8')
+            if not normalized_buffer.startswith('==>'):   
+                x, y, z = self.get_x_y_z(normalized_buffer)
+                current_vector = [x, y, z]
+                if reference_vector is None:
+                    reference_vector = current_vector
+                if current_vector and reference_vector:
+                    try:
+                        produto_interno = sum([current_vector[i] * reference_vector[i] for i in range(3)])
+                        u = sqrt(sum([current_vector[i] * current_vector[i] for i in range(3)]))
+                        v = sqrt(sum([reference_vector[i] * reference_vector[i] for i in range(3)]))
+                        angulo = acos(produto_interno/(u*v)) * 57.3 
+                    except ValueError: 
+                        angulo = 0
+                    print(round(angulo, 2))
+        
+                
+
+if __name__ == "__main__":
+    SerialIO().read()
