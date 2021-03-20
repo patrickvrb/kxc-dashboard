49bf533cb07a9fbeb166d7a412da903aa57c457c
patrickvrb <patrickvrb@gmail.com>
2021-03-20 12:19:15 -0300

    Cálculo e salvamento do ângulo (primeira medida é referência)

--- engine.py
+++ engine.py
@@ -13,7 +13,11 @@ class SerialIO():
         self.setup()
 
     def setup(self):
-        self.arduinoData = serial.Serial('COM5', 9600)
+        try:
+            self.arduinoData = serial.Serial('COM3', 9600)
+        except:
+            self.arduinoData = serial.Serial('COM5', 9600)
+            
         sleep(2)
         # mode = input('Modo? 0 para Real Time e 1 para Catch Dump\t')
         mode = '1'
@@ -51,15 +55,20 @@ class SerialIO():
                 if reference_vector is None:
                     reference_vector = current_vector
                 if current_vector and reference_vector:
-                    try:
-                        produto_interno = sum([current_vector[i] * reference_vector[i] for i in range(3)])
-                        u = sqrt(sum([current_vector[i] * current_vector[i] for i in range(3)]))
-                        v = sqrt(sum([reference_vector[i] * reference_vector[i] for i in range(3)]))
-                        angulo = acos(produto_interno/(u*v)) * 57.3 
-                    except ValueError: 
-                        angulo = 0
+                    angulo = self.angle_calc(reference_vector, current_vector)
                     print(round(angulo, 0))
                     
+    def angle_calc(self, ref_vector, vector):
+        try:
+            produto_interno = sum([vector[i] * ref_vector[i] for i in range(3)])
+            u = sqrt(sum([vector[i] * vector[i] for i in range(3)]))
+            v = sqrt(sum([ref_vector[i] * ref_vector[i] for i in range(3)]))
+            angulo = acos(produto_interno/(u*v)) * 57.3 
+        except ValueError: 
+            angulo = 0
+        return angulo
+        
+        
     def ard_dump_mode(self):
         self.arduinoData.write('17'.encode('utf-8')) # Forçando modo 17 (Config Mode)
         self.arduinoData.write('\n'.encode('utf-8'))
@@ -78,16 +87,17 @@ class SerialIO():
                 sleep(.1)
                 serial_buffer = self.arduinoData.read_until().decode('ISO-8859-1') # Ler linha a linha,  Decodificação para incluir \n, \r
                 if serial_buffer.startswith('0003'): # Primeira medida do dump
+                    ref_vector = self.get_x_y_z_dump(self.arduinoData.read_until().decode('ISO-8859-1'))
                     while not 'FFFF' in serial_buffer: # Critério de parada (último índice)
-                        serial_buffer = self.arduinoData.read_until().decode('ISO-8859-1') 
-                        x, y, z = self.get_x_y_z_dump(serial_buffer)
-                        f.write(str([x, y, z]) + '\n')
-                        print(str([x, y, z]))
+                        current_vector = self.get_x_y_z_dump(self.arduinoData.read_until().decode('ISO-8859-1'))
+                        angulo = str(self.angle_calc(ref_vector, current_vector))
+                        f.write(str(round(angulo,0)) + '\n')
+                        print(round(angulo,0))
                     f.close()
-                    sys.exit()
-           
-            
-                
+                    sys.exit()  
+    
+        
+        
 
 if __name__ == "__main__":
     SerialIO()
