c637983504188b2f4dbac5269a0eabd66589d58a
patrickvrb <patrickvrb@gmail.com>
2021-03-30 17:37:32 -0300

    Grid layout

--- dashboard.py
+++ dashboard.py
@@ -2,30 +2,58 @@ import sys  # We need sys so that we can pass argv to QApplication
 from random import randint
 
 import pyqtgraph as pg
-from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import (QApplication, QBoxLayout, QGridLayout,
+                             QHBoxLayout, QMenu, QPushButton, QVBoxLayout,
+                             QWidget)
 from pyqtgraph import PlotWidget, plot
 
 from engine import SerialIO
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QWidget):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.engine = SerialIO()
         self.engine.ard_dump_mode()  # Realiza dump do arduino em engine.py
+        self.layout_init()
+        # self.upload_data()
+
+    def layout_init(self):
+        self.list_button_init()
         self.upload_data()
+        self.setWindowTitle('KXC Dashboard')
+
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        positions = [(i, j) for i in range(2) for j in range(2)]
+
+        grid.addWidget(self.graphWidget, 0, 0)
+        grid.addWidget(self.drop_button, 2, 2)
+
+        self.move(300, 150)
+        self.setWindowTitle('KXC Dashboard')
+        self.show()
+
+    def list_button_init(self):
+        self.drop_button = QPushButton('CERVEJAS', self)
+        self.drop_button.resize(100, 32)
+        self.drop_button.move(200, 200)
+        menu = QMenu(self)
+        menu.addAction('IPA')
+        menu.addSeparator()
+        menu.addAction('LAGER')
+        menu.addSeparator()
+        menu.addAction('STOUT')
+        self.drop_button.setMenu(menu)
+        # pybutton.clicked.connect(self.clickMethod)
 
     def upload_data(self):
         self.graphWidget = pg.PlotWidget()
-        self.setCentralWidget(self.graphWidget)
-
         self.y = self.engine.get_angle_list()
-
         self.x = list(range(len(self.y)))  # y time points
-
         self.graphWidget.setBackground('w')
-
         pen = pg.mkPen(color=(255, 0, 0))
         self.data_line = self.graphWidget.plot(self.x, self.y, pen=pen)
 
@@ -42,7 +70,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     w = MainWindow()
     w.show()
     sys.exit(app.exec_())
